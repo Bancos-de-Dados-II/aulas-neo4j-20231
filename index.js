4b39aadcec96f4cc73277378481b32733be92cf5
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-06-19 16:06:45 -0300

    Adicionando método para buscar

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var driver = neo4j.driver(
     neo4j.auth.basic('neo4j', 'neo4j123*')
 );
 
-salvar();
+// salvar();
 
 async function salvar(){
     var session = driver.session();
@@ -14,6 +14,17 @@ async function salvar(){
         nome:'Maria', idade:21
     }).then(result => console.log(result.summary.counters._stats.nodesCreated))
     .catch(e => console.log(e));
+
+    session.close();
+    driver.close();
+}
+
+buscar();
+
+async function buscar(){
+    var session = driver.session();
+
+    await session.run('MATCH (p:Pessoa) RETURN p.nome as nome, p.idade as idade').then(result => console.log(result.records));
 
     session.close();
     driver.close();
